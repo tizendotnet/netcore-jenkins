9d5c3cfb6572bce63e2a15f77695e214389c7db7
Gleb Balykov <g.balykov@samsung.com>
2018-06-14 19:16:14 +0300

    Separate two options

--- jobs/generation/Utilities.groovy
+++ jobs/generation/Utilities.groovy
@@ -78,7 +78,8 @@ class Utilities {
 
           steps {
             environmentVariables {
-              env("stableOpts", "/p:StabilizePackageVersion=true /p:PackageVersionStamp=")
+              env("stableOpts", "/p:StabilizePackageVersion=true")
+              env("packageOpts", "/p:PackageVersionStamp=")
             }
           }
         }
@@ -86,12 +87,12 @@ class Utilities {
         def authorsOpts = '/p:Authors=Tizen'
 
         if (project == 'coreclr') {
-          shell("${dockerCommand} ./build.sh cross clang4.0 \${config} \${targetArch} cmakeargs -DFEATURE_GDBJIT=TRUE stripSymbols -PortableBuild=false \${buildIdOpts} -- \${stableOpts} ${authorsOpts}")
+          shell("${dockerCommand} ./build.sh cross clang4.0 \${config} \${targetArch} cmakeargs -DFEATURE_GDBJIT=TRUE stripSymbols -PortableBuild=false \${buildIdOpts} -- \${stableOpts} \${packageOpts} ${authorsOpts}")
         } else if (project == 'corefx') {
-          shell("${dockerCommand} ./build.sh -\${config} -buildArch=\${targetArch} -RuntimeOS=tizen.4.0.0 -PortableBuild=false \${buildIdOpts} -- \${stableOpts} /p:BinPlaceNETCoreAppPackage=true /p:OverridePackageSource=https:%2F%2Ftizen.myget.org/F/dotnet-core/api/v3/index.json ${authorsOpts}")
+          shell("${dockerCommand} ./build.sh -\${config} -buildArch=\${targetArch} -RuntimeOS=tizen.4.0.0 -PortableBuild=false \${buildIdOpts} -- \${stableOpts} \${packageOpts} /p:BinPlaceNETCoreAppPackage=true /p:OverridePackageSource=https:%2F%2Ftizen.myget.org/F/dotnet-core/api/v3/index.json ${authorsOpts}")
           shell("${dockerCommand} ./build-packages.sh -\${config} -ArchGroup=\${targetArch} -RuntimeOS=tizen.4.0.0 -PortableBuild=false -- ${authorsOpts}")
         } else if (project == 'core-setup') {
-          shell("${dockerCommand} ./build.sh -ConfigurationGroup=\${config} -TargetArchitecture=\${targetArch} -SkipTests=true -DisableCrossgen=true -PortableBuild=false -CrossBuild=true \${buildIdOpts} -- \${stableOpts} /p:OverridePackageSource=https:%2F%2Ftizen.myget.org/F/dotnet-core/api/v3/index.json ${authorsOpts} /p:OutputRid=tizen.4.0.0-\${targetArch}")
+          shell("${dockerCommand} ./build.sh -ConfigurationGroup=\${config} -TargetArchitecture=\${targetArch} -SkipTests=true -DisableCrossgen=true -PortableBuild=false -CrossBuild=true \${buildIdOpts} -- \${stableOpts} \${packageOpts} /p:OverridePackageSource=https:%2F%2Ftizen.myget.org/F/dotnet-core/api/v3/index.json ${authorsOpts} /p:OutputRid=tizen.4.0.0-\${targetArch}")
         }
         // Change ownership to UID of the projectDir
         // Building with docker, it will be created as root with the file it downloaded
